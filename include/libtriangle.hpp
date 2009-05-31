ec9e8c16d80aba55cf6d24644c5254684a2519f2
Francis Russell <fpr02@doc.ic.ac.uk>
2009-05-31 18:01:45 +0000

    Minor changes to modified libtriangle header file.
    Monotone-Parent: 0e6554b4138dd2da52809fa4689519191c28f00f
    Monotone-Revision: de937d83f65d15e600ce981e6e47347c459cdcd6

--- include/libtriangle.hpp
+++ include/libtriangle.hpp
@@ -1,5 +1,5 @@
-#ifndef LIBTRIANGLE_H
-#define LIBTRIANGLE_H
+#ifndef LIBTRIANGLE_HPP
+#define LIBTRIANGLE_HPP
 
 // This is copied from triangle.h in libtriangle 1.6 because the Debian modifications expose so much API that
 // it breaks the current code. Unfortunately, this might break if the triangle API changes.
@@ -7,18 +7,18 @@
 extern "C"
 {
 
-typedef double TRIREAL;
+// We assume REAL was defined as double
 
 struct triangulateio {
-  TRIREAL *pointlist;                                            /* In / out */
-  TRIREAL *pointattributelist;                                   /* In / out */
+  double *pointlist;                                             /* In / out */
+  double *pointattributelist;                                    /* In / out */
   int *pointmarkerlist;                                          /* In / out */
   int numberofpoints;                                            /* In / out */
   int numberofpointattributes;                                   /* In / out */
 
   int *trianglelist;                                             /* In / out */
-  TRIREAL *triangleattributelist;                                /* In / out */
-  TRIREAL *trianglearealist;                                      /* In only */
+  double *triangleattributelist;                                 /* In / out */
+  double *trianglearealist;                                       /* In only */
   int *neighborlist;                                             /* Out only */
   int numberoftriangles;                                         /* In / out */
   int numberofcorners;                                           /* In / out */
@@ -28,19 +28,19 @@ struct triangulateio {
   int *segmentmarkerlist;                                        /* In / out */
   int numberofsegments;                                          /* In / out */
 
-  TRIREAL *holelist;                     /* In / pointer to array copied out */
+  double *holelist;                      /* In / pointer to array copied out */
   int numberofholes;                                      /* In / copied out */
 
-  TRIREAL *regionlist;                   /* In / pointer to array copied out */
+  double *regionlist;                    /* In / pointer to array copied out */
   int numberofregions;                                    /* In / copied out */
 
   int *edgelist;                                                 /* Out only */
   int *edgemarkerlist;            /* Not used with Voronoi diagram; out only */
-  TRIREAL *normlist;             /* Used only with Voronoi diagram; out only */
+  double *normlist;              /* Used only with Voronoi diagram; out only */
   int numberofedges;                                             /* Out only */
 };
 
-void triangulate(char *, struct triangulateio *, struct triangulateio *, struct triangulateio *);
+void triangulate(char *triswitches, struct triangulateio *in, struct triangulateio *out, struct triangulateio *vorout);
 void trifree(void *memptr);
 
 }
