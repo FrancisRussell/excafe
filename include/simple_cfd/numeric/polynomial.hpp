2bf50fca7e35fd381b598da7ff8f4f550e74a895
Francis Russell <fpr02@doc.ic.ac.uk>
2009-06-30 18:51:12 +0000

    Basic polynomial manipulation.
    Monotone-Parent: 218e0067d137a50dcc8c80b30d2663b0d9fdaafb
    Monotone-Revision: d97da722706046d94e26a92800b28087108c3c3d

--- include/simple_cfd/numeric/polynomial.hpp
+++ include/simple_cfd/numeric/polynomial.hpp
@@ -5,22 +5,52 @@
 #include <string>
 #include <map>
 #include <cstddef>
+#include <algorithm>
+#include <cassert>
+#include <iosfwd>
+#include <boost/operators.hpp>
 
 namespace cfd
 {
 
-class Polynomial
+class Polynomial : boost::addable2<Polynomial, double,
+                   boost::subtractable2<Polynomial, double,
+                   boost::dividable2<Polynomial, double,
+                   boost::multipliable2<Polynomial, double, 
+                   boost::addable< Polynomial,
+                   boost::subtractable< Polynomial,
+                   boost::multipliable< Polynomial
+                   > > > > > > >
 {
 private:
+  friend std::ostream& operator<<(std::ostream& o, const Polynomial& p);
+
   typedef std::map< std::string, std::vector<std::size_t> > exponent_map_t;
   std::vector<double> coefficients;
   exponent_map_t exponentMap;
 
   void multiply(const std::string& variable, const std::size_t exponent);
   void addTerm(const double coefficient, const std::string& variable, const std::size_t exponent);
+  void addConstant(const double constant);
+  void addIndependentVariables(const Polynomial& p);
+
+  template<typename UnaryFunction>
+  void transformCoefficients(const UnaryFunction& f)
+  {
+    std::transform(coefficients.begin(), coefficients.end(), coefficients.begin(), f);
+  }
+
+  template<typename UnaryFunction>
+  void transformExponents(const std::string& variable, const UnaryFunction& f)
+  {
+    exponent_map_t::iterator eIter(exponentMap.find(variable));
+    assert(eIter != exponentMap.end());
+    std::transform(eIter->second.begin(), eIter->second.end(), eIter->second.begin(), f);
+  }
 
 public:
   Polynomial();
+  Polynomial(const Polynomial& p);
 
   explicit Polynomial(const double constant);
   explicit Polynomial(const std::string& variable);
@@ -39,13 +69,19 @@ public:
   Polynomial& operator+=(const Polynomial& p);
   Polynomial& operator-=(const Polynomial& p);
 
+  Polynomial operator-() const;
+
   Polynomial derivative(const std::size_t d) const;
+  std::size_t numTerms() const;
+  void swap(Polynomial& p);
 
   double operator()() const;
   double operator()(const double a) const;
   double operator()(const double a, const double b) const;
   double operator()(const double a, const double b, const double c) const;
 };
+
+std::ostream& operator<<(std::ostream& o, const Polynomial& p);
 
 }
 
--- lib/numeric/polynomial.cpp
+++ lib/numeric/polynomial.cpp
@@ -1,21 +1,17 @@
 #include <utility>
 #include <simple_cfd/numeric/polynomial.hpp>
+#include <boost/lambda/lambda.hpp>
+#include <cassert>
+#include <ostream>
 
 namespace cfd
 {
 
-void Polynomial::addTerm(const double coefficient, const std::string& variable, const std::size_t exponent)
+Polynomial::Polynomial()
 {
-  addIndependentVariable(variable);
-  coefficients.push_back(coefficient);
-
-  for(exponent_map_t::iterator varIter(exponentMap.begin()); varIter!=exponentMap.end(); ++varIter)
-  {
-    varIter->second.push_back(varIter->first == variable ? exponent : 0);
-  }
 }
 
-Polynomial::Polynomial()
+Polynomial::Polynomial(const Polynomial& p) : coefficients(p.coefficients), exponentMap(p.exponentMap)
 {
 }
 
@@ -44,10 +40,157 @@ Polynomial::Polynomial(const double coefficient, const std::string& variable, co
   addTerm(coefficient, variable, exponent);
 }
 
+void Polynomial::addTerm(const double coefficient, const std::string& variable, const std::size_t exponent)
+{
+  addIndependentVariable(variable);
+  coefficients.push_back(coefficient);
+
+  for(exponent_map_t::iterator varIter(exponentMap.begin()); varIter!=exponentMap.end(); ++varIter)
+  {
+    varIter->second.push_back(varIter->first == variable ? exponent : 0);
+  }
+}
+
+void Polynomial::addConstant(const double constant)
+{
+  coefficients.push_back(constant);
+
+  for(exponent_map_t::iterator varIter(exponentMap.begin()); varIter!=exponentMap.end(); ++varIter)
+  {
+    varIter->second.push_back(0);
+  }
+}
+
 void Polynomial::addIndependentVariable(const std::string& variable)
 {
   if (exponentMap.find(variable) == exponentMap.end())
     exponentMap.insert(std::make_pair(variable, std::vector<std::size_t>(coefficients.size(), 0)));
+}
+
+void Polynomial::addIndependentVariables(const Polynomial& p)
+{
+  for(exponent_map_t::const_iterator varIter(p.exponentMap.begin()); varIter!=p.exponentMap.end(); ++varIter)
+  {
+    addIndependentVariable(varIter->first);
+  }
+}
+
+Polynomial& Polynomial::operator*=(const double x)
+{
+  transformCoefficients(boost::lambda::_1 * x);
+  return *this;
+}
+
+Polynomial& Polynomial::operator/=(const double x)
+{
+  transformCoefficients(boost::lambda::_1 / x);
+  return *this;
+}
+
+Polynomial& Polynomial::operator+=(const double x)
+{
+  addConstant(x);
+  return *this;
+}
+
+Polynomial& Polynomial::operator-=(const double x)
+{
+  addConstant(-x);
+  return *this;
+}
+
+Polynomial Polynomial::operator-() const
+{
+  Polynomial result(*this);
+  result.transformCoefficients(-boost::lambda::_1);
+  return result;
+}
+
+Polynomial& Polynomial::operator*=(const Polynomial& p)
+{
+  Polynomial result;
+
+  for(std::size_t pTerm=0; pTerm<p.numTerms(); ++pTerm)
+  {
+    Polynomial multiplied(*this);
+    multiplied.transformCoefficients(boost::lambda::_1 * p.coefficients[pTerm]);
+    
+    for(exponent_map_t::const_iterator pExpIter(p.exponentMap.begin()); pExpIter!=p.exponentMap.end(); ++pExpIter)
+    {
+      multiplied.addIndependentVariable(pExpIter->first);
+      multiplied.transformExponents(pExpIter->first, boost::lambda::_1 + pExpIter->second[pTerm]);
+    }
+
+    result += multiplied;
+  }
+
+  result.swap(*this);
+
+  return *this;
+}
+
+Polynomial& Polynomial::operator+=(const Polynomial& p)
+{
+  addIndependentVariables(p);
+
+  coefficients.insert(coefficients.end(), p.coefficients.begin(), p.coefficients.end());
+
+  for(exponent_map_t::iterator varIter(exponentMap.begin()); varIter!=exponentMap.end(); ++varIter)
+  {
+    const exponent_map_t::const_iterator pIter = p.exponentMap.find(varIter->first);
+    if (pIter != p.exponentMap.end())
+    {
+      varIter->second.insert(varIter->second.end(), pIter->second.begin(), pIter->second.end());
+    }
+    else
+    {
+      varIter->second.resize(numTerms(), 0.0);
+    }
+  }
+
+  return *this;
+}
+
+Polynomial& Polynomial::operator-=(const Polynomial& p)
+{
+  *this += -p;
+  return *this;
+}
+
+std::size_t Polynomial::numTerms() const
+{
+  return coefficients.size();
+}
+
+void Polynomial::swap(Polynomial& p)
+{
+  coefficients.swap(p.coefficients);
+  exponentMap.swap(p.exponentMap);
+}
+
+std::ostream& operator<<(std::ostream& out, const Polynomial& p)
+{
+  for(std::size_t pTerm=0; pTerm<p.numTerms(); ++pTerm)
+  {
+    out << p.coefficients[pTerm];
+
+    for(Polynomial::exponent_map_t::const_iterator eIter(p.exponentMap.begin()); eIter!=p.exponentMap.end(); ++eIter)
+    {
+      if (eIter->second[pTerm] > 0)
+      {
+        out << " * ";
+        out << eIter->first;
+        
+        if (eIter->second[pTerm] != 1)
+          out << "^" << eIter->second[pTerm];
+      }
+    }
+
+    if (pTerm < p.numTerms() - 1)
+      out << " + ";
+  }
+
+  return out;
 }
 
 }
