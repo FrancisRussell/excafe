e4dff919059975ad5f4facd43afe1874a5f23738
Francis Russell <fpr02@doc.ic.ac.uk>
2009-07-13 00:20:09 +0000

    Add missing file from previous commit.
    Monotone-Parent: 051c5093f5bdd20f4064adb22005c64826c6a343
    Monotone-Revision: b6adec39ff3766a76903712fb389e0846a7fa6c4

---
+++ include/simple_cfd/dof_numbering_basic.hpp
@@ -0,0 +1,93 @@
+#ifndef SIMPLE_CFD_DOF_NUMBERING_BASIC_HPP
+#define SIMPLE_CFD_DOF_NUMBERING_BASIC_HPP
+
+#include <cstddef>
+#include <boost/array.hpp>
+#include <boost/scoped_ptr.hpp>
+#include <numeric>
+#include <utility>
+#include <cassert>
+#include "general_cell.hpp"
+#include "mesh_entity.hpp"
+
+namespace cfd
+{
+
+template<std::size_t D>
+class DofNumberingBasic
+{
+private:
+  static const std::size_t dimension = D;
+
+  boost::scoped_ptr< GeneralCell<dimension> > cell;
+  const boost::array<std::size_t, dimension+1> dofsPerEntity;
+  const boost::array<std::size_t, dimension+1> numCellEntities;
+  const std::size_t tensorSize;
+
+  static boost::array<std::size_t, dimension+1> buildNumCellEntities(const GeneralCell<dimension>& cell)
+  {
+    boost::array<std::size_t, dimension+1> numCellEntities;
+
+    for(std::size_t i=0; i<=dimension; ++i)
+      numCellEntities[i] = cell.numEntities(i);
+
+    return numCellEntities;
+  }
+
+  std::size_t numDofsPerValue() const
+  {
+    return std::inner_product(dofsPerEntity.begin(), dofsPerEntity.end(), numCellEntities.begin(), 0);
+  }
+
+public:
+  DofNumberingBasic(const GeneralCell<dimension>& _cell, const boost::array<std::size_t, dimension+1>& _dofsPerEntity,
+    const std::size_t _tensorSize) : cell(_cell.cloneGeneralCell()), dofsPerEntity(_dofsPerEntity),
+    numCellEntities(buildNumCellEntities(*cell)), tensorSize(_tensorSize)
+  {
+  }
+
+  DofNumberingBasic(const DofNumberingBasic& d) : cell(d.cell->cloneGeneralCell()),
+    dofsPerEntity(d.dofsPerEntity), numCellEntities(d.numCellEntities), tensorSize(d.tensorSize)
+  {
+  }
+
+  std::size_t getTensorIndex(const std::size_t dof) const
+  {
+    const std::size_t dofsPerValue = numDofsPerValue();
+    return dof / dofsPerValue;
+  }
+
+  std::pair<MeshEntity, std::size_t> getLocalLocation(const std::size_t dof) const
+  {
+    const std::size_t dofsPerValue = numDofsPerValue();
+    const std::size_t scalarDof = dof % dofsPerValue;
+
+    std::size_t entityDof = scalarDof;
+    for(std::size_t i=0; i<=dimension; ++i)
+    {
+      if (entityDof < dofsPerEntity[i] * numCellEntities[i])
+      {
+        const MeshEntity localEntity(i, entityDof / dofsPerEntity[i]);
+        const std::size_t localIndex(entityDof % dofsPerEntity[i]);
+
+        return std::make_pair(localEntity, localIndex);
+      }
+      else
+      {
+        entityDof -= dofsPerEntity[i] * numCellEntities[i];
+      }
+    }
+
+    assert(false && "Index out of range");
+    return std::make_pair(MeshEntity(0, 0), 0);
+  }
+
+  std::size_t numDofs() const
+  {
+    return numDofsPerValue() * tensorSize;
+  }
+};
+
+}
+
+#endif
