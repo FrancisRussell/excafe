ca4160c41399c7c6594878d69c582c9d8175533f
Francis Russell <fpr02@doc.ic.ac.uk>
2009-07-29 17:42:09 +0000

    Use BilinearFormIntegralSum object to help lifting in projection solver.
    Monotone-Parent: dbb42da72802fa678cbd119ffac5d38f36e5cfdc
    Monotone-Revision: 5712c87e485eb8ba5240a9bde7468b26922b7c21

--- include/simple_cfd/stokes_system.hpp
+++ include/simple_cfd/stokes_system.hpp
@@ -294,16 +294,15 @@ public:
     FEMatrix<dimension> linear_lhs_matrix(velocityDofMapHomogeneous, velocityDofMapHomogeneous);
     FEMatrix<dimension> linear_dirichlet_rhs_matrix(velocityDofMapHomogeneous, velocityDofMapDirichlet);
 
-    linear_lhs_matrix += 
+    BilinearFormIntegralSum linearLhsTerms = 
       B(velocity, velocity)*dx +
       B(scalar(theta * k * kinematic_viscosity) * grad(velocity), grad(velocity))*dx +
       B(scalar(-1.0 * theta * k * kinematic_viscosity) * inner(grad(velocity), n), velocity)*ds;
+
+    linear_lhs_matrix += linearLhsTerms;
     linear_lhs_matrix.assemble();
 
-    linear_dirichlet_rhs_matrix +=
-      B(velocity, velocity)*dx +
-      B(scalar(theta * k * kinematic_viscosity) * grad(velocity), grad(velocity))*dx +
-      B(scalar(-1.0 * theta * k * kinematic_viscosity) * inner(grad(velocity), n), velocity)*ds;
+    linear_dirichlet_rhs_matrix += linearLhsTerms;
     linear_dirichlet_rhs_matrix.assemble();
 
     // Add in all constant terms in the rhs matrix
