7dd60ae43c643c50fb52add802a8883db67b38e8
Francis Russell <fpr02@doc.ic.ac.uk>
2009-09-08 16:35:53 +0000

    Perform basic validation on indexed values used as r-values.
    Monotone-Parent: c84892d233d380e94767d16d0d8c7e76917a95d8
    Monotone-Revision: 379f94aaf5841e4726dc482591b784f4147b6153

--- include/simple_cfd/capture/fields/discrete_indexed_object.hpp
+++ include/simple_cfd/capture/fields/discrete_indexed_object.hpp
@@ -5,6 +5,9 @@
 #include "discrete_traits.hpp"
 #include "indexable_value.hpp"
 #include "discrete_expr_visitor.hpp"
+#include <cassert>
+#include <boost/variant/static_visitor.hpp>
+#include <boost/variant/apply_visitor.hpp>
 
 namespace cfd
 {
@@ -21,17 +24,50 @@ protected:
   parent_ptr parent;
   TemporalIndexExpr index;
 
+  class IndexValidator : public boost::static_visitor<void>
+  {
+  private:
+    const signed offsetValue;
+
+  public:
+    IndexValidator(const signed _offsetValue) : offsetValue(_offsetValue)
+    {
+    }
+
+    void operator()(const TemporalIndexOffset::absolute_tag&) const
+    {
+      assert(false && "Cannot use an absolute value when indexing an r-value");
+    }
+
+    void operator()(const TemporalIndexOffset::relative_tag&) const
+    {
+      assert(offsetValue <=0 && "Cannot refer to values in a future iteration");
+    }
+
+    void operator()(const TemporalIndexOffset::final_tag&) const
+    {
+      assert(offsetValue <=0 && "Cannot refer to values beyond the final iteration of a loop");
+    }
+  };
+
 public:
-  AbstractDiscreteObjectIndexed(const parent_ptr& _parent, const TemporalIndexExpr indexExpr) :
+  AbstractDiscreteObjectIndexed(const parent_ptr& _parent, const TemporalIndexExpr& indexExpr) :
     parent(_parent), index(indexExpr)
   {
+    assert(indexExpr.getIndex() == index.getIndex() && "Incorrect index used on expression rhs");
+    
+    TemporalIndexOffset offset = indexExpr.getOffset();
+    TemporalIndexOffset::offset_t offsetType = offset.getType();
+
+    const IndexValidator validator(offset.getValue());
+    boost::apply_visitor(validator, offsetType);
   }
 };
 
 class DiscreteIndexedScalar : public AbstractDiscreteObjectIndexed<discrete_scalar_tag>
 {
 public:
-  DiscreteIndexedScalar(const parent_ptr& _parent, const TemporalIndexExpr indexExpr) :
+  DiscreteIndexedScalar(const parent_ptr& _parent, const TemporalIndexExpr& indexExpr) :
     AbstractDiscreteObjectIndexed<discrete_scalar_tag>(_parent, indexExpr)
   {
   }
@@ -48,7 +84,7 @@ private:
   typedef DiscreteTraits<discrete_field_tag>::expr_ptr expr_ptr;
 
 public:
-  DiscreteIndexedField(const parent_ptr& _parent, const TemporalIndexExpr indexExpr) :
+  DiscreteIndexedField(const parent_ptr& _parent, const TemporalIndexExpr& indexExpr) :
     AbstractDiscreteObjectIndexed<discrete_field_tag>(_parent, indexExpr)
   {
   }
@@ -68,7 +104,7 @@ public:
 class DiscreteIndexedOperator : public AbstractDiscreteObjectIndexed<discrete_operator_tag>
 {
 public:
-  DiscreteIndexedOperator(const parent_ptr& _parent, const TemporalIndexExpr indexExpr) :
+  DiscreteIndexedOperator(const parent_ptr& _parent, const TemporalIndexExpr& indexExpr) :
     AbstractDiscreteObjectIndexed<discrete_operator_tag>(_parent, indexExpr)
   {
   }
