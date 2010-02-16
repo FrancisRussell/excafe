a80490a661ccc10b00a9d9e10332ad2fa8239d0c
Francis Russell <fpr02@doc.ic.ac.uk>
2010-02-16 18:23:58 +0000

    Implemented a lot of index manipulation for arrays of tensors of polynomials.
    Monotone-Parent: 08475e30a0ecd60306c3125fa210e64a85c9aeae
    Monotone-Revision: 70c9563d4b44113a0f238678749c679dfb3b5ecf

--- include/simple_cfd/capture/array/tensor_array_function_polynomial.hpp
+++ include/simple_cfd/capture/array/tensor_array_function_polynomial.hpp
@@ -5,11 +5,16 @@
 #include <numeric>
 #include <functional>
 #include <vector>
+#include <utility>
+#include <map>
+#include <cassert>
 #include <simple_cfd/exception.hpp>
+#include <boost/foreach.hpp>
 #include "tensor_function.hpp"
 #include "array_index.hpp"
 #include "tensor_index.hpp"
 #include "array_traits.hpp"
+#include "tensor_array_function_polynomial_visitor.hpp"
 
 namespace cfd
 {
@@ -32,6 +37,91 @@ private:
   std::set<TensorIndexID> tensorVirtualParameters;
   std::vector<polynomial_t> values;
 
+  class IndexIncrementer
+  {
+  private:
+    std::vector< std::pair<ArrayIndexID, std::size_t> > realArrayIndexExtents;
+    std::vector<TensorIndexID> realTensorIndices;
+    const std::size_t dimension;
+
+    bool increment(std::map<ArrayIndexID, std::size_t>& arrayIndex) const
+    {
+      assert(arrayIndex.size() == realArrayIndexExtents.size());
+      // Returns whether or not there was a wrap-around
+      typedef std::pair<ArrayIndexID, std::size_t> extent_t;
+      BOOST_REVERSE_FOREACH(const extent_t& extent, realArrayIndexExtents)
+      {
+        arrayIndex[extent.first] = (arrayIndex[extent.first] + 1) % extent.second;
+
+        if (arrayIndex[extent.first] != 0)
+          return false;
+      }
+      
+      return true;
+    }
+
+    bool increment(std::map<TensorIndexID, std::size_t>& tensorIndex) const
+    {
+      assert(tensorIndex.size() == realTensorIndices.size());
+      // Returns whether or not there was a wrap-around
+      BOOST_REVERSE_FOREACH(const TensorIndexID& id, realTensorIndices)
+      {
+        tensorIndex[id] = (tensorIndex[id] + 1) % dimension;
+
+        if (tensorIndex[id] != 0)
+          return false;
+      }
+
+      return true;
+    }
+
+  public:
+    IndexIncrementer(const TensorArrayFunctionPolynomial& parent) : dimension(parent.dimension)
+    {
+      for(std::size_t i=0; i<parent.arrayExtents.numIndices(); ++i)
+      {
+        if (!parent.isVirtual(parent.arrayIndexParameters[i]))
+          realArrayIndexExtents.push_back(std::make_pair(parent.arrayIndexParameters[i], parent.arrayExtents[i]));
+      }
+
+      for(std::size_t i=0; i<parent.rank; ++i)
+      {
+        if (!parent.isVirtual(parent.tensorIndexParameters[i]))
+          realTensorIndices.push_back(parent.tensorIndexParameters[i]);
+      }
+    }
+
+    void zero(std::map<ArrayIndexID, std::size_t>& arrayIndex) const
+    {
+      arrayIndex.clear();
+
+      typedef std::pair<ArrayIndexID, std::size_t> extent_t;
+      BOOST_FOREACH(const extent_t& extent, realArrayIndexExtents)
+      {
+        arrayIndex.insert(std::make_pair(extent.first, 0));
+      }
+    }
+
+    void zero(std::map<TensorIndexID, std::size_t>& tensorIndex) const
+    {
+      tensorIndex.clear();
+
+      BOOST_FOREACH(const TensorIndexID& id, realTensorIndices)
+      {
+        tensorIndex.insert(std::make_pair(id, 0));
+      }
+    }
+
+    bool increment(std::map<ArrayIndexID, std::size_t>& arrayIndex, 
+      std::map<TensorIndexID, std::size_t>& tensorIndex) const
+    {
+      if (increment(tensorIndex))
+        return increment(arrayIndex);
+      else
+        return false;
+    }
+  };
+
   std::size_t getInternalRank() const
   {
     return rank - tensorVirtualParameters.size();
@@ -48,11 +138,21 @@ private:
 
     for(std::size_t i=0; i<rank; ++i)
     {
-      if (!real || tensorVirtualParameters.find(tensorIndexParameters[i]) != tensorVirtualParameters.end())
+      if (!real || tensorVirtualParameters.find(tensorIndexParameters[i]) == tensorVirtualParameters.end())
         result *= dimension;
     }
 
     return result;
+  }
+
+  bool isVirtual(const ArrayIndexID& index) const
+  {
+    return arrayVirtualParameters.find(index) != arrayVirtualParameters.end();
+  }
+
+  bool isVirtual(const TensorIndexID& index) const
+  {
+    return tensorVirtualParameters.find(index) != tensorVirtualParameters.end();
   }
 
   std::size_t tensorExtentVirtual() const
@@ -71,7 +171,7 @@ private:
 
     for(std::size_t i=0; i<arrayExtents.numIndices(); ++i)
     {
-      if (!real || arrayVirtualParameters.find(arrayIndexParameters[i]) != arrayVirtualParameters.end())
+      if (!real || arrayVirtualParameters.find(arrayIndexParameters[i]) == arrayVirtualParameters.end())
         result *= arrayExtents[i];
     }
 
@@ -98,47 +198,108 @@ private:
     return arrayExtentReal() * tensorExtentReal();
   }
 
-  std::size_t flattenVirtual(const ArrayIndex<fixed_tag>& arrayIndex) const
+  std::size_t flattenReal(const ArrayIndex<fixed_tag>& arrayIndex) const
   {
-    assert(arrayIndex.numIndices() ==  arrayExtents.numIndices());
+    assert(arrayIndex.numIndices() == getNumInternalArrayIndices());
     
     std::size_t offset = 0;
     std::size_t multiplier = 1;
+    std::size_t realIndex = arrayIndex.numIndices() - 1;
 
-    for(int i=arrayIndex.numIndices()-1; i>=0; --i)
+    for(int i=arrayExtents.numIndices()-1; i>=0; --i)
     {
-      offset += arrayIndex[i] * multiplier;
-      multiplier *= arrayExtents[i];
+      if (arrayVirtualParameters.find(arrayIndexParameters[i]) == arrayVirtualParameters.end())
+      {
+        offset += arrayIndex[realIndex] * multiplier;
+        multiplier *= arrayExtents[i];
+        --realIndex;
+      }
     }
 
+    assert(realIndex = 0);
     return offset;
   }
 
-  std::size_t flattenVirtual(const TensorIndex<fixed_tag>& tensorIndex) const
+  std::size_t flattenReal(const TensorIndex<fixed_tag>& tensorIndex) const
   {
-    assert(tensorIndex.getRank() == rank);
+    assert(tensorIndex.getRank() == getInternalRank());
     assert(tensorIndex.getDimension() == dimension);
 
     std::size_t offset = 0;
     std::size_t multiplier = 1;
 
-    for(int i=rank-1; i>=0; --i)
+    for(int i=getInternalRank()-1; i>=0; --i)
     {
-      offset += tensorIndex[i] * multiplier;
-      multiplier *= dimension;
+        offset += tensorIndex[i] * multiplier;
+        multiplier *= dimension;
     }
 
     return offset;
   }
 
-  std::size_t flattenVirtual(const ArrayIndex<fixed_tag>& arrayIndex, const TensorIndex<fixed_tag>& tensorIndex) const
+  std::size_t flattenReal(const std::map<ArrayIndexID, std::size_t>& arrayIndex) const
   {
-    return flattenVirtual(arrayIndex) * tensorExtentVirtual() + flattenVirtual(tensorIndex);
+    assert(arrayIndex.size() >= getNumInternalArrayIndices());
+    
+    std::size_t offset = 0;
+    std::size_t multiplier = 1;
+
+    for(int i=arrayExtents.numIndices()-1; i>=0; --i)
+    {
+      if (arrayVirtualParameters.find(arrayIndexParameters[i]) == arrayVirtualParameters.end())
+      {
+        const std::map<ArrayIndexID, std::size_t>::const_iterator arrIndexIter = arrayIndex.find(arrayIndexParameters[i]);
+        if (arrIndexIter == arrayIndex.end())
+        {
+          CFD_EXCEPTION("Missing array index value when flattening.");
+        }
+        else
+        {
+          offset += arrIndexIter->second * multiplier;
+          multiplier *= arrayExtents[i];
+        }
+      }
+    }
+
+    return offset;
+  }
+
+  std::size_t flattenReal(const std::map<TensorIndexID, std::size_t>& tensorIndex) const
+  {
+    assert(tensorIndex.size() >= getInternalRank());
+    
+    std::size_t offset = 0;
+    std::size_t multiplier = 1;
+
+    for(int i=rank-1; i>=0; --i)
+    {
+      if (tensorVirtualParameters.find(tensorIndexParameters[i]) == tensorVirtualParameters.end())
+      {
+        const std::map<TensorIndexID, std::size_t>::const_iterator tensorIndexIter = tensorIndex.find(tensorIndexParameters[i]);
+        if (tensorIndexIter == tensorIndex.end())
+        {
+          CFD_EXCEPTION("Missing tensor index value when flattening.");
+        }
+        else
+        {
+          offset += tensorIndexIter->second * multiplier;
+          multiplier *= dimension;
+        }
+      }
+    }
+
+    return offset;
   }
 
   std::size_t flattenReal(const ArrayIndex<fixed_tag>& arrayIndex, const TensorIndex<fixed_tag>& tensorIndex) const
   {
-    //FIXME: implement me!
+    return flattenReal(arrayIndex) * tensorExtentReal() + flattenReal(tensorIndex);
+  }
+
+  std::size_t flattenReal(const std::map<ArrayIndexID, std::size_t>& arrayIndex, 
+    const std::map<TensorIndexID, std::size_t>& tensorIndex) const
+  {
+    return flattenReal(arrayIndex) * tensorExtentReal() + flattenReal(tensorIndex);
   }
 
   TensorFunction::ref expand(const std::set<ArrayIndexID>& arrayIndicesToExpand, const
@@ -163,7 +324,11 @@ public:
     const std::size_t _dimension) :
     arrayExtents(_arrayExtents), rank(_rank), dimension(_dimension), values(extentReal())
   {
-    // FIXME: create indices
+    for(std::size_t i=0; i<arrayExtents.numIndices(); ++i)
+       arrayIndexParameters.push_back(ArrayIndexID(i));
+
+    for(std::size_t i=0; i<rank; ++i)
+       tensorIndexParameters.push_back(TensorIndexID(i));
   }
 
   virtual std::size_t getTensorRank() const
@@ -186,6 +351,22 @@ public:
     return arrayExtents[index];
   }
 
+  void accept(TensorArrayFunctionPolynomialVisitor& visitor)
+  {
+    IndexIncrementer incrementer(*this);
+    std::map<ArrayIndexID, std::size_t> arrayIndex; 
+    std::map<TensorIndexID, std::size_t> tensorIndex;
+
+    incrementer.zero(arrayIndex);
+    incrementer.zero(tensorIndex);
+
+    do
+    {
+      visitor.visit(arrayIndex, tensorIndex, (*this)(arrayIndex, tensorIndex));
+    }
+    while(!incrementer.increment(arrayIndex, tensorIndex));
+  }
+
   virtual TensorFunction::ref differentiate(const ScalarReference& reference) const
   {
     if (reference.isBound()) 
@@ -201,6 +382,13 @@ public:
   }
 
   polynomial_t& operator()(const ArrayIndex<fixed_tag>& arrayIndex, const TensorIndex<fixed_tag>& tensorIndex)
+  {
+    const std::size_t offset = flattenReal(arrayIndex, tensorIndex);
+    return values[offset];
+  }
+
+  polynomial_t& operator()(const std::map<ArrayIndexID, std::size_t>& arrayIndex, 
+    const std::map<TensorIndexID, std::size_t>& tensorIndex)
   {
     const std::size_t offset = flattenReal(arrayIndex, tensorIndex);
     return values[offset];
---
+++ include/simple_cfd/capture/array/tensor_array_function_polynomial_visitor.hpp
@@ -0,0 +1,27 @@
+#ifndef SIMPLE_CFD_CAPTURE_ARRAY_TENSOR_ARRAY_FUNCTION_POLYNOMIAL_VISITOR_HPP
+#define SIMPLE_CFD_CAPTURE_ARRAY_TENSOR_ARRAY_FUNCTION_POLYNOMIAL_VISITOR_HPP
+
+#include <map>
+#include <cstddef>
+#include "parameter_identifiers.hpp"
+#include "tensor_function.hpp"
+
+namespace cfd
+{
+
+namespace detail
+{
+
+class TensorArrayFunctionPolynomialVisitor
+{
+public:
+  virtual void visit(const std::map<ArrayIndexID, std::size_t>& arrayIndex, 
+    const std::map<TensorIndexID, std::size_t>& tensorIndex,
+    TensorFunction::polynomial_t) = 0;
+};
+
+}
+
+}
+
+#endif
