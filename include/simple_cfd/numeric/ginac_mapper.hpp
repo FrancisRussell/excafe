17fec383e9f42b5f211d1456cd763da175d31795
Francis Russell <francis@unchartedbackwaters.co.uk>
2010-05-05 09:53:48 +0000

    Fix bug caused by bizarre semantics of GiNaC::ex comparison operators.
    Monotone-Parent: 03c2cbdbb84ffe6ab43320c6b465e2f9a7a905df
    Monotone-Revision: aae6203995fefeec66c4743318381801d5aa791b

--- include/simple_cfd/numeric/ginac_mapper.hpp
+++ include/simple_cfd/numeric/ginac_mapper.hpp
@@ -2,7 +2,9 @@
 #define SIMPLE_CFD_NUMERIC_GINAC_MAPPER_HPP
 
 #include <sstream>
-#include <boost/bimap.hpp>
+#include <map>
+#include <utility>
+#include <boost/noncopyable.hpp>
 #include <simple_cfd/exception.hpp>
 #include <ginac/symbol.h>
 
@@ -13,13 +15,16 @@ namespace detail
 {
 
 template<typename T>
-class GinacMapper
+class GinacMapper : boost::noncopyable
 {
 private:
   typedef T key_type;
   typedef GiNaC::symbol value_type;
 
-  boost::bimap<key_type, value_type> mappings;
+  std::map<key_type, value_type> mappings;
+  
+  // NOTE: we need to use a GiNaC::ex_is_less to get a sensible ordering
+  std::map<value_type, key_type, GiNaC::ex_is_less> reverse_mappings;
 
   struct object_creator
   {
@@ -42,9 +47,9 @@ public:
 
   value_type getGiNaCSymbol(const key_type& k)
   {
-    const typename boost::bimap<key_type, value_type>::left_iterator iter = mappings.left.find(k);
+    const typename std::map<key_type, value_type>::const_iterator iter = mappings.find(k);
 
-    if (iter != mappings.left.end())
+    if (iter != mappings.end())
     {
       return iter->second;
     }
@@ -53,16 +58,17 @@ public:
       std::ostringstream name;
       name << k;
       const value_type symbol(name.str());
-      mappings.left.insert(std::make_pair(k, symbol));
+      mappings.insert(std::make_pair(k, symbol));
+      reverse_mappings.insert(std::make_pair(symbol, k));
       return symbol;
     }
   }
 
   key_type getOriginalSymbol(const value_type& v) const
   {
-    const typename boost::bimap<key_type, value_type>::right_const_iterator iter = mappings.right.find(v);
+    const typename std::map<value_type, key_type>::const_iterator iter = reverse_mappings.find(v);
 
-    if (iter != mappings.right.end())
+    if (iter != reverse_mappings.end())
     {
       return iter->second;
     }
