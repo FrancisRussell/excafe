cfeba53794b1655fb532a3a36f41619488ba0271
Francis Russell <francis@unchartedbackwaters.co.uk>
2011-03-29 17:53:30 +0000

    Add a test to exercise prime factoriser.
    Monotone-Parent: 64ebd6a94f36fb9decfb3b24e521d60b90cb01de
    Monotone-Revision: 2532429a9e1e381e8d7b05b4ebbe4db63e72276f

---
+++ tests/factoriser.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <simple_cfd/numeric/factoriser.hpp>
+#include <cln/cln.h>
+#include <boost/utility.hpp>
+#include <iostream>
+
+void factor(const cln::cl_I& n)
+{
+  using namespace cfd;
+
+  Factoriser factoriser;
+  const std::vector<Factoriser::power_t> result = factoriser.factor(n);
+
+  std::cout << "Factors of " << n << ": ";
+
+  for (std::vector<Factoriser::power_t>::const_iterator 
+       iter=result.begin(); iter!=result.end(); 
+       ++iter)
+  {
+    std::cout << iter->first << "^" << iter->second;
+    if (boost::next(iter) != result.end())
+      std::cout << ", ";
+  }
+  std::cout << std::endl;
+}
+
+
+int main(int argc, char** argv)
+{
+  // Factoring 0 should return an explicit 0.
+  factor(0);
+
+  // Factoring 1 should return nothing.
+  factor(1);
+
+  // Factoring 48 should return 7^2.
+  factor(49);
+
+  // Factoring 53 shoudl return itself.
+  factor(53);
+
+  // Factoring a negative value.
+  factor(153);
+  factor(-153);
+}
