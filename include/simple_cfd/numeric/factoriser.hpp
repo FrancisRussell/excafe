a5ce5db6747053019bd4ea093b64811f22075560
Francis Russell <francis@unchartedbackwaters.co.uk>
2011-03-29 16:14:12 +0000

    Improve prime factoriser implementation.
    A list of known factors is used to avoid unnecessary searches and the
    implementation as been moved into cpp files.
    
    Monotone-Parent: c054c47e18930daebf3784b670b8d36239982e62
    Monotone-Revision: 1218fd37f7f0e2bf3b9dc3029036f0ca7149945d

--- include/simple_cfd/numeric/factoriser.hpp
+++ include/simple_cfd/numeric/factoriser.hpp
@@ -12,47 +12,11 @@ class Factoriser
 {
 public:
   typedef std::pair<cln::cl_I, cln::cl_I> power_t;
+  std::vector<power_t> factor(const cln::cl_I& n);
 
-  std::vector<power_t> factor(const cln::cl_I& n)
-  {
-    using namespace cln;
-
-    std::vector< std::pair<cl_I, cl_I> > result;
-    cl_I value = n;
-
-    // We explicitly handle the zero case since an empty list of factors is assumed to mean 1.
-    if (value == 0)
-    {
-      result.push_back(power_t(0, 1));
-      return result;
-    }
-
-    // We generate -1 as a factor for any negative numbers.
-    if (value < 0)
-    {
-      value = -value;
-      result.push_back(power_t(-1, 1));
-    }
-
-    cl_I factor(2);
-
-    while(value != 1)
-    {
-      cl_I exponent = 0;
-      while (rem(value, factor) == 0)
-      {
-        value /= factor;
-        ++exponent;
-      }
-
-      if (exponent > 0)
-        result.push_back(power_t(factor, exponent));
-
-      ++factor;
-    }
-
-    return result;
-  }
+private:
+  static const int primes[];
+  cln::cl_I removeFactor(cln::cl_I& value, const cln::cl_I& factor) const;
 };
 
 }
