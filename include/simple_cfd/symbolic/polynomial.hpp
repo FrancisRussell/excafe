fed172289e64461fc68933f75a9de09411e54b6d
Francis Russell <francis@unchartedbackwaters.co.uk>
2011-04-26 17:01:28 +0000

    Move some Polynomial implementation into cpp file.
    Monotone-Parent: 75fd7a7b40e21b3e772ee199b213c508a65c3f51
    Monotone-Revision: b8f93320eb0118ccd742267b060c27429f1ce172

--- include/simple_cfd/symbolic/polynomial.hpp
+++ include/simple_cfd/symbolic/polynomial.hpp
@@ -7,15 +7,11 @@
 #include <ostream>
 #include <cassert>
 #include <iterator>
-#include <stack>
-#include <queue>
 #include <functional>
 #include <boost/iterator/iterator_facade.hpp>
 #include <boost/operators.hpp>
 #include <boost/foreach.hpp>
 #include <simple_cfd/symbolic/symbol.hpp>
-#include <simple_cfd/symbolic/sum.hpp>
-#include <simple_cfd/symbolic/product.hpp>
 #include <simple_cfd/symbolic/expr.hpp>
 
 namespace cfd
@@ -309,6 +305,9 @@ private:
   template<typename monomial_t>
   void pushMonomialTemplated(const Rational& c, const monomial_t& m)
   {
+    if (c == 0)
+      return;
+
     if (numTerms() == 0 || m != (*this)[numTerms()-1])
     {
       coefficients.push_back(c);
@@ -318,6 +317,13 @@ private:
     else
     {
       coefficients[numTerms()-1] += c;
+
+      if (coefficients[numTerms()-1] == 0)
+      {
+        exponents.erase(exponents.begin() + monomialOffsets[numTerms()-1], exponents.end());
+        coefficients.resize(coefficients.size()-1);
+        monomialOffsets.resize(monomialOffsets.size()-1);
+      }
     }
   }
 
@@ -332,126 +338,22 @@ private:
   }
       
 public:
-  Polynomial()
-  {
-    monomialOffsets.push_back(0);
-  }
-
-  Polynomial(const Rational& r)
-  {
-    coefficients.push_back(r);
-    monomialOffsets.push_back(0);
-    monomialOffsets.push_back(0);
-  }
-
-  Polynomial(const Symbol& s)
-  {
-    coefficients.push_back(Rational(1));
-    monomialOffsets.push_back(0);
-    monomialOffsets.push_back(1);
-    exponents.push_back(exponent_t(s, 1));
-  }
-
-  Polynomial operator+(const Polynomial& b) const
-  {
-    Polynomial result;
-
-    const Polynomial& a = *this;
-    const_iterator aIter = a.begin();
-    const_iterator bIter = b.begin();
-
-    while(aIter != a.end() || bIter != b.end())
-    {
-      if (aIter == a.end())
-      {
-        result.pushMonomial(bIter.coefficient(), *bIter);
-        ++bIter;
-      }
-      else if (bIter == b.end())
-      {
-        result.pushMonomial(aIter.coefficient(), *aIter);
-        ++aIter;
-      }
-      else if (*aIter < *bIter)
-      {
-        result.pushMonomial(aIter.coefficient(), *aIter);
-        ++aIter;
-      }
-      else
-      {
-        result.pushMonomial(bIter.coefficient(), *bIter);
-        ++bIter;
-      }
-    }
-
-    return result;
-  }
-
-  Polynomial operator*(const Polynomial& b) const
-  {
-    typedef std::pair<MonomialProduct, std::size_t> state_t;
-
-    Polynomial result;
-    std::vector<const_iterator> positions(numTerms(), b.begin());
-    std::priority_queue<state_t, 
-                        std::vector<state_t>,
-                        std::greater<state_t> > queue;
-
-    if (b.begin() != b.end())
-    {
-      for(std::size_t i=0; i<numTerms(); ++i)
-        queue.push(std::make_pair((*this)[i]*(*positions[i]), i));
-    }
-
-    while(!queue.empty())
-    {
-      const std::pair<MonomialProduct, std::size_t> top = queue.top();
-      queue.pop();
-
-      const int position = top.second;
-      result.pushMonomial(positions[position].coefficient() * coefficients[position], top.first);
-      ++positions[position];
-
-      if (positions[position] != b.end())
-        queue.push(std::make_pair((*this)[top.second]*(*positions[top.second]), top.second));
-    }
-
-    return result;
-  }
-
-  Expr toExpr() const
-  {
-    Sum result;
-    for(const_iterator polyIter = begin(); polyIter != end(); ++polyIter)
-    {
-      Product term = Product::constant(polyIter.coefficient());
-
-      BOOST_FOREACH(const exponent_t& exponent, *polyIter)
-        term *= Product::pow(exponent.first, exponent.second);
-
-      result += term;
-    }
-
-    return result.simplify();
-  }
+  Polynomial();
+  Polynomial(const Rational& r);
+  Polynomial(const Symbol& s);
 
   std::size_t numTerms() const
   {
     return coefficients.size();
   }
 
-  void write(std::ostream& out) const
-  {
-    for(const_iterator iter = begin(); iter != end(); ++iter)
-    {
-      out << iter.coefficient();
-      iter->write(out);
-
-      if (iter+1 != end())
-        out << " + ";
-    }
-  }
+  Polynomial operator+(const Polynomial& b) const;
+  Polynomial operator*(const Polynomial& b) const;
+  void write(std::ostream& out) const;
+  Expr toExpr() const;
 };
+
+std::ostream& operator<<(std::ostream& o, const Polynomial& p);
 
 }
 
--- lib/symbolic/polynomial.cpp
+++ lib/symbolic/polynomial.cpp
@@ -1,3 +1,7 @@
+#include <queue>
+#include <utility>
+#include <simple_cfd/symbolic/sum.hpp>
+#include <simple_cfd/symbolic/product.hpp>
 #include <simple_cfd/symbolic/polynomial.hpp>
 
 namespace cfd
@@ -6,10 +10,130 @@ namespace cfd
 namespace symbolic
 {
 
+Polynomial::Polynomial()
+{
+  monomialOffsets.push_back(0);
+}
+
+Polynomial::Polynomial(const Rational& r)
+{
+  coefficients.push_back(r);
+  monomialOffsets.push_back(0);
+  monomialOffsets.push_back(0);
+}
+
+Polynomial::Polynomial(const Symbol& s)
+{
+  coefficients.push_back(Rational(1));
+  monomialOffsets.push_back(0);
+  monomialOffsets.push_back(1);
+  exponents.push_back(exponent_t(s, 1));
+}
+
+Polynomial Polynomial::operator+(const Polynomial& b) const
+{
+  Polynomial result;
+
+  const Polynomial& a = *this;
+  const_iterator aIter = a.begin();
+  const_iterator bIter = b.begin();
+
+  while(aIter != a.end() || bIter != b.end())
+  {
+    if (aIter == a.end())
+    {
+      result.pushMonomial(bIter.coefficient(), *bIter);
+      ++bIter;
+    }
+    else if (bIter == b.end())
+    {
+      result.pushMonomial(aIter.coefficient(), *aIter);
+      ++aIter;
+    }
+    else if (*aIter < *bIter)
+    {
+      result.pushMonomial(aIter.coefficient(), *aIter);
+      ++aIter;
+    }
+    else
+    {
+      result.pushMonomial(bIter.coefficient(), *bIter);
+      ++bIter;
+    }
+  }
+
+  return result;
+}
 
 Polynomial::MonomialProduct Polynomial::Monomial::operator*(const Monomial& m) const
 {
   return MonomialProduct(*this, m);
+}
+
+Polynomial Polynomial::operator*(const Polynomial& b) const
+{
+  typedef std::pair<MonomialProduct, std::size_t> state_t;
+
+  Polynomial result;
+  std::vector<const_iterator> positions(numTerms(), b.begin());
+  std::priority_queue<state_t, 
+                      std::vector<state_t>,
+                      std::greater<state_t> > queue;
+
+  if (b.begin() != b.end())
+  {
+    for(std::size_t i=0; i<numTerms(); ++i)
+      queue.push(std::make_pair((*this)[i]*(*positions[i]), i));
+  }
+
+  while(!queue.empty())
+  {
+    const std::pair<MonomialProduct, std::size_t> top = queue.top();
+    queue.pop();
+
+    const int position = top.second;
+    result.pushMonomial(positions[position].coefficient() * coefficients[position], top.first);
+    ++positions[position];
+
+    if (positions[position] != b.end())
+      queue.push(std::make_pair((*this)[top.second]*(*positions[top.second]), top.second));
+  }
+
+  return result;
+}
+
+Expr Polynomial::toExpr() const
+{
+  Sum result;
+  for(const_iterator polyIter = begin(); polyIter != end(); ++polyIter)
+  {
+    Product term = Product::constant(polyIter.coefficient());
+
+    BOOST_FOREACH(const exponent_t& exponent, *polyIter)
+      term *= Product::pow(exponent.first, exponent.second);
+
+    result += term;
+  }
+
+  return result.simplify();
+}
+
+void Polynomial::write(std::ostream& out) const
+{
+  for(const_iterator iter = begin(); iter != end(); ++iter)
+  {
+    out << iter.coefficient();
+    iter->write(out);
+
+    if (iter+1 != end())
+      out << " + ";
+  }
+}
+
+std::ostream& operator<<(std::ostream& o, const Polynomial& p)
+{
+  p.write(o);
+  return o;
 }
 
 }
