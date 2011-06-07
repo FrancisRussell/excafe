a684a59ca739820765ac6ef122cb928890e063c2
Francis Russell <francis@unchartedbackwaters.co.uk>
2011-06-07 22:17:57 +0000

    Add additional expression conversion tests.
    Monotone-Parent: f164affbd4eda23fec861d27c1dea5ebd40fff1c
    Monotone-Revision: a6b7f8f4e936b91e19d2f0e3f814a0e2b7442f51

--- tests/expression_conversion.cpp
+++ tests/expression_conversion.cpp
@@ -3,34 +3,64 @@
 #include <simple_cfd/numeric/convert_expression.hpp>
 #include <simple_cfd/numeric/polynomial_fraction.hpp>
 #include <simple_cfd/numeric/ginac_expression.hpp>
+#include <simple_cfd/numeric/excafe_expression.hpp>
+
+
+template<typename T>
+struct TypeNames
+{
+};
+
+template<typename V>
+struct TypeNames< cfd::PolynomialFraction<V> >
+{
+  static std::string name() { return "PolynomialFraction"; }
+};
+
+template<typename V>
+struct TypeNames< cfd::GinacExpression<V> >
+{
+  static std::string name() { return "GinacExpression"; }
+};
+
+template<typename V>
+struct TypeNames< cfd::ExcafeExpression<V> >
+{
+  static std::string name() { return "ExcafeExpression"; }
+};
 
 template<typename from, typename to>
-void test(const from& f)
+void printConversion(const from& f)
 {
   std::cout << f << " -> " << cfd::detail::convert_expression<to>(f) << std::endl;
 }
 
 template<typename from, typename to>
-void test()
+void printConversions()
 {
-  test<from, to>(from(7.5));
-  test<from, to>(from("x") + from("y"));
-  test<from, to>(from(7, "k", 5));
-  test<from, to>(from(4, "k", 2) + from(2, "x", 3));
-  test<from, to>(from(2, "k", 2) / from(6, "x", 3));
-  test<from, to>(from(4, "k", 2) * from(1, "x", 2));
+  std::cout << "Converting from " << TypeNames<from>::name() << " to " << TypeNames<to>::name() << ":" << std::endl;
+  printConversion<from, to>(from(7.5));
+  printConversion<from, to>(from("x") + from("y"));
+  printConversion<from, to>(from(7, "k", 5));
+  printConversion<from, to>(from(4, "k", 2) + from(2, "x", 3));
+  printConversion<from, to>(from(2, "k", 2) / from(6, "x", 3));
+  printConversion<from, to>(from(4, "k", 2) * from(1, "x", 2));
+  std::cout << std::endl;
 }
-
 
 int main(int, char**)
 {
   using namespace cfd;
 
-  std::cout << "Converting from PolynomialFraction to GinacExpression:" << std::endl; 
-  test< PolynomialFraction<std::string>, GinacExpression<std::string> >();
+  printConversions< PolynomialFraction<std::string>, PolynomialFraction<std::string> >();
+  printConversions< PolynomialFraction<std::string>, GinacExpression<std::string> >();
+  printConversions< PolynomialFraction<std::string>, ExcafeExpression<std::string> >();
 
-  std::cout << std::endl;
+  printConversions< GinacExpression<std::string>, PolynomialFraction<std::string> >();
+  printConversions< GinacExpression<std::string>, GinacExpression<std::string> >();
+  printConversions< GinacExpression<std::string>, ExcafeExpression<std::string> >();
 
-  std::cout << "Converting from GinacExpression to PolynomialFraction:" << std::endl; 
-  test< GinacExpression<std::string>, PolynomialFraction<std::string> >();
+  printConversions< ExcafeExpression<std::string>, PolynomialFraction<std::string> >();
+  printConversions< ExcafeExpression<std::string>, GinacExpression<std::string> >();
+  printConversions< ExcafeExpression<std::string>, ExcafeExpression<std::string> >();
 }
