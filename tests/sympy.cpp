da43d134266c3a2d513ec030d920e8fffcd344f5
Francis Russell <francis@unchartedbackwaters.co.uk>
2012-05-08 23:55:43 +0000

    Support absolute values in SymPy bridge.
    Monotone-Parent: 3b3b34d5473eb14e0eb6c40b8b0b6690c69a8b88
    Monotone-Revision: 47ed735d3ede42eeff6aaf1d858361641de98b3d

--- tests/sympy.cpp
+++ tests/sympy.cpp
@@ -3,7 +3,7 @@
 #include <boost/python/detail/wrap_python.hpp>
 #include <simple_cfd/python_manager.hpp>
 #include <simple_cfd/numeric/sympy_expression_converter.hpp>
-#include <simple_cfd/numeric/ginac_expression.hpp>
+#include <simple_cfd/numeric/excafe_expression.hpp>
 #include <simple_cfd/numeric/sympy_expression.hpp>
 #include <simple_cfd/numeric/convert_expression.hpp>
 
@@ -11,18 +11,18 @@ int main(int, char**)
 {
   using namespace cfd;
 
-  typedef GinacExpression<std::string> poly_t;
-  poly_t poly = poly_t(7, "k", 5) + poly_t(4, "x", 2);
+  typedef ExcafeExpression<std::string> poly_t;
+  poly_t poly = poly_t(7, "k", 5) + poly_t(4, "x", 2) + abs(poly_t(2, "y"));
 
   PythonManager::instance().init();
 
   try
   {
     SymPyExpression<std::string> sympyExpr(poly);
-    std::cout << "GiNaC expression: " << poly << std::endl;
-    std::cout << "After GiNaC -> SymPy: " << sympyExpr << std::endl;
-    std::cout << "After SymPy -> GiNaC: ";
-    std::cout << cfd::detail::convert_expression< GinacExpression<std::string> >(sympyExpr) << std::endl;
+    std::cout << "Excafe expression: " << poly << std::endl;
+    std::cout << "After Excafe -> SymPy: " << sympyExpr << std::endl;
+    std::cout << "After SymPy -> Excafe: ";
+    std::cout << cfd::detail::convert_expression< ExcafeExpression<std::string> >(sympyExpr) << std::endl;
 
     std::cout << "Integrated w.r.t. x: " << sympyExpr.integrate("x") << std::endl;
     std::cout << "Integrated w.r.t. x (0<x<1): " << sympyExpr.integrate("x", 0, 1) << std::endl;
